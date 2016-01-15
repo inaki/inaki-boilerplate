d65a42abf4fd4b77ca0a258376bcd862f1cfa589
inaki <iiaranzadi@gmail.com>
2016-01-15 01:09:41 -0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,1 @@
+$("#toggle").click(function(){$(this).toggleClass("active"),$("#overlay").toggleClass("open")}),$("#overlay a").click(function(){$("#toggle").toggleClass("active"),$("#overlay").toggleClass("open")});
